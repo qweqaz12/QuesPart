e15ed8c79e7f93f5cf64e46637298e31eee943b7
qweqaz12 <42571428+qweqaz12@users.noreply.github.com>
2019-06-13 16:55:14 +0800

    Create Pojo.cs

---
+++ Assets/Scripts/Pojo.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Pojo
+{
+    public class Roletype
+    {
+        public long Id { get; set; }
+        public string roletype { get;set;}
+    }
+
+     public class BaseQuestion
+     {
+         public long ID { get; set; }
+         public string QuesID { get; set; }
+         public string Title { get; set; }
+         public string QusOption { get; set; }
+         public int OptiCount { get; set; }
+         public string CorrAns { get; set; }
+         public string AnsGroup { get; set; }
+         public string Score { get; set; }
+         public int IsReject { get; set; }
+         public string ExamTime { get; set; }
+     }
+
+    public class SingleQuestion : BaseQuestion
+    {
+        public string QuesSort { get; set; }
+    }
+
+    public class MultiQuestion : BaseQuestion
+    {
+        public string  AnsStyle{ get; set; }
+    }
+
+}
+
